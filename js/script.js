4971b80693b13a4e4e97bc60e979422795474362
lizzie-abuladze <liziabuladze54@gmail.com>
2025-03-26 01:52:40 +0400

    js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let fetchDiv = document.getElementById("container");
 let btnLoadMore = document.querySelector(".more");
 let currentPage = 1;
 let nameList = document.querySelector(".fNameList");
+let btnLoadPrev = document.querySelector(".previous");
+let totalPages;
 
 function getFname(page) {
   fetch("https://reqres.in/api/users?page=" + page, {
@@ -23,10 +25,23 @@ function getFname(page) {
 
       resJs.data.forEach((element) => {
         let li = document.createElement("li");
-        li.textContent = `${element.first_name}`;
+
+        let fName = document.createElement("h2");
+        fName.textContent = `${element.first_name}`;
+
+        let img = document.createElement("img");
+        img.src = element.avatar;
+
+        li.appendChild(img);
+        li.appendChild(fName);
         fragment.appendChild(li);
       });
+      nameList.innerHTML = " ";
       nameList.appendChild(fragment);
+
+      totalPages = resJs.total_pages;
+
+      changeBtnStatus();
     })
 
     .catch(function (error) {
@@ -41,14 +56,40 @@ function getFname(page) {
       } else {
         let pError3 = document.createElement("p");
         pError3.innerText = "check your internet connection";
-        fetchDiv.appendChild(pError3)
+        fetchDiv.appendChild(pError3);
       }
     });
 }
 
-getFname(1);
+getFname();
 
-btnLoadMore.addEventListener("click", function () {
-  currentPage++;
+btnLoadPrev.addEventListener("click", function () {
+  if (currentPage === 1) {
+    return;
+  }
+  currentPage -= 1;
   getFname(currentPage);
 });
+
+btnLoadMore.addEventListener("click", function () {
+  if (currentPage == totalPages) {
+    return;
+  }
+  currentPage += 1;
+  getFname(currentPage);
+});
+
+function changeBtnStatus() {
+  if (currentPage === 1) {
+    btnLoadPrev.disabled = true;
+  } else {
+    btnLoadPrev.disabled = false;
+  }
+
+  if (currentPage === totalPages) {
+    btnLoadMore.disabled = true;
+  } else {
+    btnLoadMore.disabled = false;
+  }
+}
+
